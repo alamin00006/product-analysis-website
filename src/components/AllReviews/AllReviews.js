0022255f41414f8664dfa230738baa079405810c
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-04-05 13:19:07 +0600

    review image added

--- src/components/AllReviews/AllReviews.js
+++ src/components/AllReviews/AllReviews.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './AllReviews.css'
 
 const AllReviews = (props) => {
-    const {name, rattings, reviews} = props.review;
+    const {name, rattings, reviews, image} = props.review;
     return (
         
         
         <div className="card-body card-container">
-        <h5 className="card-title">{name}</h5>
+            <div>
+                <img src={image} alt="" />
+            </div>
+         <div>
+       <h5 className="card-title">{name}</h5>
         <p className="card-text">{reviews}</p>
         <p className="card-text text-danger">{rattings}</p>
+       </div>
         </div>
        
     );
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,7 +4,7 @@ import './Review.css'
 const Review = (props) => {
  
     const {review} = props;
- const {name, rattings, reviews} = review;
+ const {name, rattings, reviews,image} = review;
  
 
     return (
@@ -12,9 +12,14 @@ const Review = (props) => {
      <div className='review-info'>
        
         <div className="card-body card-container">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text"> {reviews}</p>
-        <p className="card-text rattings">{rattings}</p>
+           <div>
+           <img src={image} alt="" />
+           </div>
+            <div>
+              <h5 className="card-title">{name}</h5>
+                  <p className="card-text"> {reviews}</p>
+                      <p className="card-text rattings">{rattings}</p>
+            </div>
   
         </div>
   </div>
